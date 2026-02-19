b4ebb38a332f0d3ff41daebe7cf22dc36966a072
Fernando Cuesta <fernandocuestab@gmail.com>
2026-02-19 23:49:54 +0000

    [FEAT] Importa get_next_prime en test_calculator y elimina import redundante

--- src/backend/tests/test_calculator.py
+++ src/backend/tests/test_calculator.py
@@ -1,4 +1,5 @@
 from src.backend.calculator import is_prime
+from src.backend.calculator import get_next_prime
 
 
 def test_is_prime_basic():
@@ -8,5 +9,4 @@ def test_is_prime_basic():
 
 
 def test_get_next_prime_after_10_is_11():
-    from src.backend.calculator import get_next_prime
     assert get_next_prime(10) == 11
